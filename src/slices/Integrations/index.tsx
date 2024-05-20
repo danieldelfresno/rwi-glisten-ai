3b2ab1c17d0ace6d5f2b4bb698b4d3c3edcea241
Daniel Fresno <daniel.del.fresno@gmail.com>
2024-05-19 21:04:22 -0300

    UPD: Gsap animations

--- src/slices/Integrations/index.tsx
+++ src/slices/Integrations/index.tsx
@@ -9,6 +9,8 @@ import { FaDigitalOcean, FaCloudflare, FaNpm, FaGithub, FaFigma, FaFly } from "r
 import StylizedLogoMark from "./StylizedLogoMark"
 import React from "react";
 import clsx from "clsx";
+import AnimatedContent from "./AnimatedContent";
+import { useGSAP } from "@gsap/react";
 
 /**
  * Props for `Integrations`.
@@ -27,6 +29,7 @@ const Integrations = ({ slice }: IntegrationsProps): JSX.Element => {
     figma: <FaFigma />,
     fly: <FaFly />,
   }
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
@@ -42,24 +45,7 @@ const Integrations = ({ slice }: IntegrationsProps): JSX.Element => {
         <div className="mx-auto mt-6 max-w-md text-balance text-center text-slate-300">
           <PrismicRichText field={slice.primary.body} />
         </div>
-        <div className="mt-20 flex flex-col items-center md:flex-row">
-          {slice.items.map((item, index) => (
-            <React.Fragment key={index}>
-              {index === Math.floor(slice.items.length / 2) && (
-                <>
-                  <StylizedLogoMark />
-                  <div className="signal-line rotate-180 bg-gradient-to-t" />
-                </>
-              )}
-              <div className="pulsing-icon flex aspect-square shrink-0 items-center justify-center rounded-full border border-blue-50/30 bg-blue-50/25 p-3 text-3xl text-blue-100 opacity-40 md:text-4xl lg:text-5xl">
-                {item.icon && icons[item.icon]}
-              </div>
-              {index !== slice.items.length - 1 && (
-                <div className={clsx("signal-line", index >= Math.floor(slice.items.length / 2) ? "rotate-180" : "rotate-0")} />
-              )}
-            </React.Fragment>
-          ))}
-        </div>
+        <AnimatedContent slice={slice} />
       </div>
     </Bounded>
   );
